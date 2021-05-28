b4c74c2078bbe7693ee8d8f3edc1500fb7317cd9
owner <owner@192.168.1.3>
2021-05-28 22:02:41 +0900

    GitHubの学習

---
+++ GitHubSample/src/test/ArithmeticSample.java
@@ -0,0 +1,13 @@
+package test;
+
+public class ArithmeticSample {
+
+	public int count(int num) {
+
+		//足し算を40回繰り返す
+		for (int cnt = 0; cnt < 40; cnt++) {
+			num++;
+		}
+		return num;
+	}
+}
---
+++ GitHubSample/src/test/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ArithmeticSample cnt = new ArithmeticSample();
+		int ans = cnt.count(5);
+
+		//計算結果を出力する
+		System.out.println(ans);
+	}
+
+}
