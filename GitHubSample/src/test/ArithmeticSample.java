b0291eab6901f6fbfc2a7c6be5f5f63a3f39ec4f
owner <owner@192.168.1.3>
2021-05-28 22:33:08 +0900

    再修正済

--- GitHubSample/src/test/ArithmeticSample.java
+++ GitHubSample/src/test/ArithmeticSample.java
@@ -5,8 +5,8 @@ public class ArithmeticSample {
 	public int count(int num) {
 
 
-		//足し算を60回繰り返す
-		for (int cnt = 0; cnt < 60; cnt++) {
+		//足し算を70回繰り返す
+		for (int cnt = 0; cnt < 70; cnt++) {
 			num++;
 		}
 		return num;
